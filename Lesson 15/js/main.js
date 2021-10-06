c10a858f094535119ed768f2a62eb1e838bba2bf
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-06 21:42:43 +0500

    Half lesson 15

---
+++ Lesson 15/js/main.js
@@ -0,0 +1,20 @@
+//* var, let, const
+//! var
+//! var => variable buni xohlagancha o'zgartirsa bo'ladi
+// var x = 1
+// var price_1 = 20
+// price_1 = 90
+// var price_1 = "Hello"
+// console.log(price_1);
+// var priceOne = 40
+// var price_one = 45
+// console.log(x)
+// x=200
+// console.log(x)
+// var y = 3
+// y=100
+// var z = x*y
+// var Ibrohim = 20
+// var user1 = "Ibrohim"
+// console.log(user1)
+//! let
