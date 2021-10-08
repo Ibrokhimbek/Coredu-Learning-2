c82dd870583f73fcb12e354ab117e22e2a77fb9c
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-08 11:55:54 +0500

    Lesson 16

---
+++ Lesson 16/js/main.js
@@ -0,0 +1,49 @@
+//* Data types => Strings, numbers, booleans, undefined, null, array, object
+//! Strings
+// let user = "Ibrohim" //! Type 1
+// let user2 = 'Behruz' //! Type 2
+// let user3 = `Ibrohim Jalilov` //! Type 3
+// console.log(user3);
+//! Numbers
+// let number = 50
+// console.log(number);
+//! Booleans
+// let is18 = true
+// let isMerried = false
+// console.log(isMerried);
+//! undefined
+// let uni
+// console.log(uni);
+//! null
+// let isMerried = null
+// console.log(isMerried);
+//! array
+// const cars = ["Matiz", "Tiko", "Maluba", "Cabolot", "Nexia", {
+//     car: {
+//         caption: "RedLine Kaptiva",
+//         color: "red",
+//         tire: 5,
+//         fuel: "Gaz",
+//         tanirovka: true
+//     }
+// }]
+// const data = [undefined, true, false, 39, null, "Hello"]
+// const user = ["Ibrohim Turaboyev", 20, true, "TESLA", "MBM"]
+// console.log(cars);
+//! object
+// let object = {
+//     car: {
+//         caption: "RedLine Kaptiva",
+//         color: "red",
+//         tire: 5, 
+//         fuel: "Gaz",
+//         tanirovka: true
+//     },
+//     isMarried: false,
+//     kids: 15,
+//     phoneNumber: +998935133417,
+//     array: [
+//         "Hello", "Hi", "Bor yo'qol", "Bevafo dunyo", "Xo'rlangan dasturchi"
+//     ],
+// }
+// console.log(object);
