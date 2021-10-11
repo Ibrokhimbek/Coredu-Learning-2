e255c75f760d19f9b4df423bfdc52bd70c7ccc4f
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-11 11:45:48 +0500

    Lesson 17

---
+++ Lesson 17/js/main.js
@@ -0,0 +1,33 @@
+//! variable lar joylashuvi bo'yicha 2turga bo'linadi => global, local
+// let x = 50 //* => Global
+// function getCoffee() {
+//     let x = 5 //* Local
+//     let y = 6 //* Local
+//     console.log(x+y)
+// }
+
+// function hello() {
+//     console.log(x);
+// }
+// getCoffee()
+// hello()
+
+//! Parametrlar
+// function myAge(a, b) {
+//     return a-b
+// }
+// const Ibrohim = myAge(2021, 2002)
+// console.log(Ibrohim);
+
+// function fullName(name, surname, age) {
+//     return name+" "+surname+" is "+age+" years old"
+// }
+// const ism = "Ibrohim"
+// const familiya = 'Turaboyev'
+// const yosh = 19
+// const user = fullName(ism, familiya, yosh)
+// console.log(user);
+
+function gazigaBos() {
+    console.log("Hello Behruz")
+}
