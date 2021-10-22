15de0cd194a145a15344942e8a8f7dfab1485518
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-22 20:02:22 +0500

    Lesson 20

---
+++ Lesson 20/js/main.js
@@ -0,0 +1,30 @@
+// const div = document.querySelector("#ibrohim").innerHTML
+// const countOfDiv = div.length //* length method
+
+// const text = "Biz \"Ibrohim\" degan bolani ko'rdik"
+// const text2 = 'Biz "Ibrohim" degan bolani ko\'rdik'
+// console.log(text2);
+
+// const text = "Hello Ibrohim! \nWe knew, we waited you are awasome instructor of Full stack web development!"
+// //* /n yangi qatorga tushirib beradi
+// console.log(text);
+
+// const Ibrohim =
+// document.querySelector("#ibrohim")
+
+// document.querySelector("#ibrohim").innerHTML = "Hello Ibrohim! \
+// We knew, we waited you are awasome instructor of \
+//  Full stack web development!"
+
+// const text = document.querySelector("#ibrohim")
+// const hello = "Salom"
+// const world = "Dunyo"
+// text.innerHTML = "Salom " + "Dunyo"
+// text.innerHTML = hello +" "+ world
+// text.innerHTML = `${hello} Ibrohim! Bu ${world} judayam go'zal!`
+
+// let string = new String("Ibrohim")
+// let string2 = "Ibrohim"
+// let x = '6'
+// let y = '5'
+// console.log(x === y)
