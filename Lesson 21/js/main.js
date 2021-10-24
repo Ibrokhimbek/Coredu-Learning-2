79ad49ce1e23fbd0e0c4136fa9076c306e8796bd
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-24 15:04:36 +0500

    Lesson 21

---
+++ Lesson 21/js/main.js
@@ -0,0 +1,29 @@
+const text = "My, name. is "
+// const text = "       My name is Ibrohim. My surname is also IBROHIM           "
+// const text2 = "Yangi O'zbekiston!!! Odalat qilish kerak, 5 Tashabbus,"
+// const text3 = "Yangi O'zbekistonni mardikorlar bilan quramiz!!! Uraaa GMni tanla Milliy mahsulot, Milliy g'urur"
+// console.log(text.slice(3, 7));
+// console.log(text.slice(-7));
+// console.log(text.substring(3,7))
+// console.log(text.substr(7, 4))
+// console.log(text.substr(-7))
+
+// let pText = document.querySelector("#text").innerHTML
+// const cutText = pText.substr(0, 65)
+// document.querySelector('#text').innerHTML = cutText+"..."
+
+// const replacedString = text.replace(/iBrOhIm/i, "Behruz_tot_samiy")
+// console.log(replacedString)
+
+// const replacedString = text.replace(/Ibrohim/g, "Ibrohim_qabristanskiy")
+// console.log(replacedString)
+
+// console.log(text.toUpperCase())
+// console.log(text.toLowerCase())
+// console.log(text.concat(". ", text2," ", text3))
+// console.log(text.trim())
+// console.log(text.padStart(18, "Ibrohim"));
+// console.log(text.padEnd(18, "Ibrohim"));
+// console.log(text.charAt(5))
+// console.log(text[0])
+// console.log(text.split(","))
