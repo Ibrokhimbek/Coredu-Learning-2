e21ed2a71466ea69c7f32cd51b14af935f49f879
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-25 11:24:59 +0500

    Lesson 22

---
+++ Lesson 22/js/main.js
@@ -0,0 +1,41 @@
+// const text = "Yangi O'zbekistonni mardikorlar bilan quramiz!"
+// const text = `
+// Assalamu alaikum! Do'stlar bugun saylov uchastkasiga xush kelibsiz! Biz sizni ko'rganimizdan xursandmiz?
+// `
+// console.log(text.includes("Yangi", 6));
+
+// const x = 3.14
+// const y = 3
+// const x = 3425434e5
+// const y = 342e-5
+// const x = 20
+// const y = 10
+// const z = "25"
+// console.log(x+y);  // 2010
+// console.log(y+x);  // 1020
+// console.log(+x+y); // 30
+// const result = x+y
+// const lastResult = +z+result
+// console.log(lastResult);
+
+// let x = "100";
+// let y = "10";
+// let z = (+x) + (+y);
+// console.log(z);
+
+// let x = 100 / "Apple";
+// console.log(isNaN(x));
+
+// console.log(2/0) //* Infinity
+// console.log(-2/0) //* -Infinity
+
+// let x = 224 //* Number
+// let obj = new Number(224) //* Object
+// console.log(x==obj);
+
+// let x = new Number(500);             
+// let y = new Number(500);
+// console.log(x==y);  
+
+// let x = 345.23456224
+// console.log(parseInt(x));
