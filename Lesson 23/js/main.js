3d14a109d701dc2732477b8e44f46a7c46fdb3d0
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-26 11:00:14 +0500

    Lesson 23

---
+++ Lesson 23/js/main.js
@@ -0,0 +1,30 @@
+// let x = 3.14
+// let y = x.toString()
+// console.log(x)
+// console.log(parseFloat(y))
+// let z = x + (+y)
+// let z = + y + x
+// console.log(z);
+
+// let x = 20
+// let y = "Banana"
+// console.log(x/y);
+
+// let cars = ["Tiko", "Damas", "Kamaz", "Eshshak", "Teshik to'g'ora"]
+
+// console.log(cars[4]);
+
+// const users = ["Ibrohim", "Behruz", "Sardor", "Qo'ymurod", "Ketmonjon"]
+// const users = []
+// users[0] = "Dasturchi"
+// users[1] = 324
+// console.log(users);
+
+
+// const users = ["Ibrohim", "Behruz", "Sardor", "Qo'ymurod", "Ketmonjon"]
+// users[users.length] = "Ivan"
+// users[users.length] = "Peter"
+// let thirdName = users[2]
+// console.log(users);
+// console.log(users.length);
+// console.log(users[users.length - 2])
