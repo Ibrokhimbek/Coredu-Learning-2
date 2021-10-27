b3a35079ad5425067a912a09ccbe2e6d349b397d
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-27 11:58:39 +0500

    Lesson 24

---
+++ Lesson 24/js/main.js
@@ -0,0 +1,27 @@
+// const tools = ["VScode", "JS", "React", "CSS", "Bootstrap"]
+// document.querySelector("#result").innerHTML = tools.join(" /Ustozimiz zo'r/ ")
+
+// const motto = ["Yangi O'zbekiston", "Biz kutgandik", "Musaffo osmon", ]
+// motto.pop() //! pop method of an Array => eng oxirgi elementni olib tashlaydi
+// const x = motto.pop()
+// console.log(x);
+// let y = "Odalat qilaylik"
+// let x = motto.push("Odalat qilaylik") //! push method of an Array => eng oxiridan element qo'shadi
+// console.log(motto);
+// console.log(motto[x-1]);
+
+// motto.shift() //! shift method => Boshidan element olb tashlaydi
+// let x = motto.shift()
+// motto.unshift("80.01%")
+// let x = motto.unshift("80.01%")
+// motto[1] = "80.01%"
+// motto[motto.length] = "80.01%"
+
+// delete motto[motto.length-1]
+
+// console.log(motto.length);
+// console.log(motto[0]);
+
+// const cars = ["Matiz", "Tiko", "Damas", "AUDI", "BMW"]
+// cars.splice(2,2, "Gentra 1.5")
+// console.log(cars);
