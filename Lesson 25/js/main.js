f52fe7cccdfecde4ff8acb38b9312fe064a2cf88
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-28 20:41:30 +0500

    Lesson 25

---
+++ Lesson 25/js/main.js
@@ -0,0 +1,38 @@
+// let boys = ["Males", "Erkaklar", "Mujiki"]
+// let girls = ["Females", "Ayollar", "Djenshini"]
+// let others = ["LGBT"]
+// console.log(girls.concat(boys, others));
+
+// let arr = ['Tiko', 'Matiz']
+// let arr2 = ["Bevafo dunyo"]
+// console.log(arr.concat("Spark", "Hello", arr2));
+
+// let arr = ['Bexa_tot_samiy', "abraham", "Parijon", "Palonpiston", "Gulla, Yashna", 'Lalaku']
+// let newArr = arr.slice(1,3)
+// console.log(arr);
+// console.log(newArr);
+
+// let array = ["Behruz", "Ibrohim", "Sunnatilla", "Mirbek", 'Salohiddin', "Timur", "Bahodir", "Quvondiq", "O'tkir", "Abraham"]
+// const sortedArray = array.sort()
+// console.log(sortedArray);
+
+// console.log(array.reverse());
+
+// let numbers = [30, 24, 56, 2, 4, 79, 120, 5476, 22, 11, 33, 33]
+// let omadliInson = numbers.sort(function (a, b) { return 0.5 - Math.random() })[0]
+// console.log(omadliInson);
+// console.log(numbers.sort(function(a, b) {return b-a}));
+
+// const cars = [
+//     { name: "Volga", year: 2004 },
+//     { name: "Matiz", year: 2000 },
+//     { name: "Tiko", year: 2002 }
+// ]
+// const sortedCars = cars.sort(function (a, b) {
+//     let x = a.name.toLowerCase()
+//     let y = b.name.toLowerCase()
+//     if (x < y) { return -1 }
+//     if (x > y) { return 1 }
+// })
+// console.log(cars.sort(function (a, b) { return a.year - b.year }));
+// console.log(sortedCars);
