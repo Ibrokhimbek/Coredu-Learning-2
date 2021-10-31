8d3a585ec8475bf77087850f43cce2a29363d5a7
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-10-31 13:15:50 +0500

    Lesson 26

---
+++ Lesson 26/js/main.js
@@ -0,0 +1,33 @@
+// const array = ["Ibrohim", "Behruz", 'Sardor', "Mirbek", "Sunnatilla"]
+// const list = document.querySelector("#list")
+
+// for (let i = 0; i < array.length; i++) {
+//     const li = `
+//         <li>${array[i]}</li>
+//     `
+//     list.innerHTML += li 
+// }
+
+// array.forEach((data, index, array) => {
+//     console.log(data, index, array);
+//     const val = "Salom "+data
+//     const li = `
+//         <li>${val} / ${index}</li>
+//     `
+//     list.innerHTML += li
+// })
+
+// const values = [10, 7, 4, 5, 6, 3, 1]
+// console.log(values);
+// const newMapArray = values.map(data => {
+//     return data * 2
+// })
+// console.log(newMapArray);
+
+// const array = ["Ibrohim", "Behruz", 'Sardor', "Mirbek", "Sunnatilla", 56, 55]
+
+// const newArray = array.filter(data => {
+//     return typeof(data) == typeof("8")
+// })
+
+// console.log(newArray);
