a140056eee98aa7f0ead7b60aad1ef6246ab315a
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-11-03 12:32:41 +0500

    Lesson 27

---
+++ Lesson 27/js/main.js
@@ -0,0 +1,34 @@
+// let x = 8
+// const y = 8
+
+// if (x == y) {
+//     console.log('Chiqdi')
+// }
+// else {
+//     console.log("oxirgi najotman");
+// }
+
+// let light = false
+// document.querySelector("#turner").innerHTML = "Turn on"
+
+// const flashlight = () => {
+//     if (light == false) {
+//         document.querySelector('#lamp').innerHTML = "Svet Yondi"
+//         document.querySelector("#turner").innerHTML = "Turn off"
+//         light = true
+//     } else {
+//         document.querySelector("#lamp").innerHTML = "Svet O'chdi"
+//         document.querySelector('#turner').innerHTML = "Turn on"
+//         light = false
+//     }
+// }
+
+// if (x == y) {
+//     console.log("X va Y qiymatlar teng")
+// } else if (x > y) {
+//     console.log("X Y qiymatdan katta");
+// } else if (y > x) {
+//     console.log("Y X qiymatdan katta");
+// }
+
+// x==y ? console.log("qiymatlar teng") : console.log("Yo X qiymat yo Y qiymat katta");
