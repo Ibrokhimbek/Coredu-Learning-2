06416edff72d54a710158a84d1f2d1303e04d403
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-11-06 11:37:39 +0500

    Lesson 28

---
+++ Lesson 28/js/main.js
@@ -0,0 +1,15 @@
+// const fullDate = new Date()
+// console.log(fullDate);
+// // document.querySelector("#result").innerHTML = fullDate
+
+// const day = fullDate.getDay()
+// document.querySelector("#day").innerHTML = "Day is "+day
+
+// const month = fullDate.getMonth()+1
+// document.querySelector("#month").innerHTML = "Month is "+month
+
+// const year = fullDate.getFullYear()
+// document.querySelector("#year").innerHTML = "Date is "+year
+
+// const number = Math.floor(Math.random() * 11)
+// console.log(number);
