3fa0a97ea152419fc1e8c547580e9a6412d7e691
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-11-09 19:27:09 +0500

    Lesson 31

---
+++ Lesson 31/js/main.js
@@ -0,0 +1,28 @@
+// const result = document.getElementById("result")
+// const btn = document.querySelector("#blueBtn")
+// let classAdded = false
+// function switcher() {
+//     // result.style.fontSize = "80px"
+//     // result.style.color = "red"
+//     // result.style.fontWeight = "bold"
+//     // result.style = "color: blue; font-size: 80px;"
+
+//     if (classAdded == false) {
+//         btn.classList.add("blueBtn")
+//         classAdded = true
+//     } else {
+//         btn.classList.remove("blueBtn")
+//         classAdded = false
+//     }
+// }
+
+// function switcher() {
+//     btn.classList.toggle("blueBtn")
+// }
+
+// const btn = document.querySelector("#blueBtn")
+// const result = document.getElementById("result")
+
+// btn.addEventListener("click", () => {
+//     result.classList.toggle("blueBtn")
+// })
