2782eb4979cf69b29684ceb47f834e64625c4285
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-11-11 19:31:35 +0500

    Lesson 32

--- Lesson 32/js/main.js
+++ Lesson 32/js/main.js
@@ -1,12 +1,21 @@
 const opener = document.querySelector(".open_btn")
 const closer = document.querySelector(".close_btn")
+const listItems = document.querySelectorAll(".nav_list-item")
+console.log(listItems);
 
 opener.addEventListener("click", () => {
     document.querySelector(".nav_list").classList.remove("closedList")
     document.querySelector(".nav_list").classList.add("openedList")
+    listItems.forEach((item, index) => {
+        item.style.animation = `listAnimation 0.3s forwards ease-in ${index / 5}s`
+        console.log(index/5);
+    })
 })
 
 closer.addEventListener("click", () => {
     document.querySelector(".nav_list").classList.remove("openedList")
     document.querySelector(".nav_list").classList.add("closedList")
+    listItems.forEach((item, index) => {
+        item.style.animation = `listRemove 0.3s forwards ease-in ${index / 5}s`
+    })
 })
