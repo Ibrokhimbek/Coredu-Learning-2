915701812a4c1bb35003f2e8ee838abefae1182b
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-11-12 18:14:12 +0500

    Lesson 34

---
+++ Lesson 34/js/main.js
@@ -0,0 +1,19 @@
+// setTimeout(() => {
+//     console.log("Hello")
+// }, 5000)
+
+// setInterval(() => {
+//     console.log("Salom")
+// }, 3000)
+
+// const result = document.querySelector("#result")
+// const startBtn = document.querySelector("#start")
+// const stopBtn = document.querySelector("#stop")
+// let number = 0
+
+// startBtn.addEventListener("click", () => {
+//     counter()
+// })
+// stopBtn.addEventListener("click", () => {
+//     clearInterval(counter)
+// })
